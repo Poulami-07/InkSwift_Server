ec7ab838f8874ec4ee968306b83f677d13ea5cf2
Poulami-07 <poulami@example.com>
2025-07-17 23:58:10 +0530

    Fixed cors

--- index.js
+++ index.js
@@ -26,15 +26,30 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 const allowedOrigins = [
-  'https://ink-swift-client.vercel.app/'
-]; 
+  "https://ink-swift-client-3xlrb8958-poulami-gandhis-projects.vercel.app",
+  "https://ink-swift-client.vercel.app/",
+  "http://localhost:5173"  
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
   
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: allowedOrigins, 
-  credentials: true}));
+// app.use(cors({
+//   origin: allowedOrigins, 
+//   credentials: true}));
 app.use(cookieParser());
 
 app.use('/api/docs', docRoute);
